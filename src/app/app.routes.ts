79e7a38a46aef70df385a5e51700bd80e523585d
Tarun Gupta <99278334+WebxTechx@users.noreply.github.com>
2023-12-25 16:47:34 +0530

    Details page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,38 +7,58 @@ import { MaintainceComponent } from './shared/maintaince/maintaince.component';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { ContactComponent } from './contact/contact.component';
 import { ServicePageComponent } from './service-page/service-page.component';
+import { BlogDetailComponent } from './blog-detail/blog-detail.component';
+import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
+    title: 'The Tarun Gupta',
   },
   {
     path: 'about-me',
     component: AboutPageComponent,
+    title: 'About Tarun',
   },
   {
     path: 'portfolio',
     component: PortfolioPageComponent,
+    title: "Tarun's Portfolio",
+  },
+  {
+    path: 'portfolio-detail',
+    component: PortfolioDetailComponent,
+    title: 'Portolfio',
   },
   {
     path: 'blog',
     component: BlogPageComponent,
+    title: 'Blog',
+  },
+  {
+    path: 'blog-detail',
+    component: BlogDetailComponent,
+    title: 'Blog',
   },
   {
     path: 'services',
     component: ServicePageComponent,
+    title: 'Services',
   },
   {
     path: 'contact',
     component: ContactComponent,
+    title: 'Contact',
   },
   {
     path: 'coming-soon',
     component: MaintainceComponent,
+    title: 'Coming Soon',
   },
   {
     path: '**',
     component: NotFoundComponent,
+    title: 'Not Found',
   },
 ];
--- src/app/shared/not-found/not-found.component.ts
+++ src/app/shared/not-found/not-found.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { GlobalService } from '../../services/global.service';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
   imports: [],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.sass'
+  styleUrl: './not-found.component.sass',
 })
 export class NotFoundComponent {
-
+  constructor(public globalService: GlobalService) {}
 }
